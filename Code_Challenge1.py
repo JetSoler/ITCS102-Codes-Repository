4cb9bea8c37499147ecf21e31e156b5c01c40a97
JetSoler <solerjetmigs2@gmail.com>
2026-08-13 12:59:14 +0800

    Add files via upload

---
+++ Code_Challenge1.py
@@ -0,0 +1,11 @@
+
+star = "\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+star1 = "\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*"
+star3 = "\n\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t*"
+star4 = "\n\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+star5 = "\n\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+
+
+
+
+print(star, star1, star3, star4, star5)
