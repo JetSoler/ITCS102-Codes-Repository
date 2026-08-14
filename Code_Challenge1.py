26584f70da40e394cda01a792aefcc84863b50a0
JetSoler <solerjetmigs2@gmail.com>
2026-08-14 20:18:29 +0800

    Update Code_Challenge1.py

--- Code_Challenge1.py
+++ Code_Challenge1.py
@@ -1,11 +1,19 @@
-
 star = "\t\t\t\t\t\t\t\t\t\t\t\t\t*"
-star1 = "\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*"
-star3 = "\n\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t*"
-star4 = "\n\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*"
-star5 = "\n\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+star1 = "\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*"
+star3 = "\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*"
+star4 = "\n\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t*"
+star5 = "\n\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+star6 = "\n\t\t\t*"
+Name = "\t\t\t\t\t\t\t\tHI! My Name is Jet Migs C. Soler"
+star6_2 = "\t\t\t\t\t\t\t\t*"
+star7 = "\n\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+star8 = "\n\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t*"
+star9 = "\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*"
+star10 = "\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*"
+star11 = "\n\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+space = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 
 
 
 
-print(star, star1, star3, star4, star5)
+print(space, star, star1, star3, star4, star5, star6, Name, star6_2, star7, star8, star9, star10, star11)
