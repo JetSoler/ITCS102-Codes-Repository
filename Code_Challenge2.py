c888b670db009cbbebd06404ad7bedcbd36bfe77
JetSoler <solerjetmigs2@gmail.com>
2026-08-27 20:15:32 +0800

    Add files via upload

---
+++ Code_Challenge2.py
@@ -0,0 +1,49 @@
+# Variables:
+Money = 2312
+a = 1000
+b = 500
+c = 200
+d = 100
+e = 50
+f = 20
+g = 10
+h = 5
+i = 1
+
+# This if for the account balance
+print("Balance =", Money)
+
+# This is for the 1000
+print("1000 =", Money//a)
+
+# This is for the 500
+FiveH = Money%a
+print("500 =", FiveH//b)
+
+# This is for the 200
+TwoH = Money%b
+print("200 =", TwoH//c)
+
+# This is for the 100
+OneH = Money%c
+print("100 =", OneH//d)
+
+# This is for the 50
+Pipty = Money%d
+print("50 =", Pipty//e)
+
+# This is for the 20
+Binti = Money%e
+print("20 =", Binti//f)
+
+# This is for the 10
+Gis = Money%f
+print("10 =", Gis//g)
+
+# This is for the 5
+Lima = Money%g
+print("5 =", Lima//h)
+
+# This is for the 1
+isa = Money%h
+print("1 =", isa//i)
