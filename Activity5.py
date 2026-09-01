5f1b744c30c8b8524dfa03d92d3a0e26973cc44b
JetSoler <solerjetmigs2@gmail.com>
2026-09-01 12:30:51 +0800

    Add files via upload

---
+++ Activity5.py
@@ -0,0 +1,19 @@
+# + - addition
+# - - subtraction
+# * - multiplication
+# / - division
+# // - floor division
+# ** - exponentation
+# % - modulus
+
+a = 45
+b = 23
+sum = a+b
+
+print("The sum of a + b is ", sum)
+print("The sum of a - b is ", 45-23)
+print("The sum of a * b is ", 45*23)
+print("The sum of a / b is ", 45/23)
+print("The sum of a // b is ", 45//23)
+print("The sum of a ** b is ", 45**23)
+print("The sum of a % b is ", 45%23)
