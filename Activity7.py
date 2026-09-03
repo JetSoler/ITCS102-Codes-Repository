5764cbd841100b308eb8d1a7a67cdb9ee03748bd
JetSoler <solerjetmigs2@gmail.com>
2026-09-03 12:11:24 +0800

    Add files via upload

---
+++ Activity7.py
@@ -0,0 +1,34 @@
+total = 0
+
+numb = eval(input("Enter A Number ----->  "))
+total += numb
+
+numb = eval(input("Enter A Number ----->  "))
+total **= numb
+
+numb = eval(input("Enter A Number ----->  "))
+total += numb
+
+numb = eval(input("Enter A Number ----->  "))
+total %= numb
+
+numb = eval(input("Enter A Number ----->  "))
+total /= numb
+
+numb = eval(input("Enter A Number ----->  "))
+total -= numb
+
+numb = eval(input("Enter A Number ----->  "))
+total -= numb
+
+numb = eval(input("Enter A Number ----->  "))
+total *= numb
+
+numb = eval(input("Enter A Number ----->  "))
+total += numb
+
+numb = eval(input("Enter A Number ----->  "))
+total /= numb
+
+
+print("The sum is", total)
