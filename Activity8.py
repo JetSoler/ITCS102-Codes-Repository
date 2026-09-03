9820f22cd1020a9adfff0d46d0a8a8991ccaa0ad
JetSoler <solerjetmigs2@gmail.com>
2026-09-03 12:35:45 +0800

    Add files via upload

---
+++ Activity8.py
@@ -0,0 +1,11 @@
+hobbies = ""
+
+
+h = input("Give Atleast 3 Of Your Hobbies?  ")
+hobbies += h + ", "
+h = input("Give Atleast 3 Of Your Hobbies?  ")
+hobbies += h + ", "
+h = input("Give Atleast 3 Of Your Hobbies?  ")
+hobbies += h + ", "
+
+print("So your hobbies are:",hobbies)
