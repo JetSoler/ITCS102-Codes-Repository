f4faa7a50b3f6568c595326b21b92aed75e6d84c
JetSoler <solerjetmigs2@gmail.com>
2026-09-08 12:40:05 +0800

    Add files via upload

---
+++ Activity12.py
@@ -0,0 +1,11 @@
+import getpass
+username = "Jetmigs"
+password = "akolangtoguys"
+
+u = input(" Please input your name  ")
+p = getpass.getpass(" Please input your password  ")
+
+if username == u and p == password :
+	print(" Nice one ")
+else :
+	print(" Wehh hecker ka noh ")
