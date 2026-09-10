fdce176b6d85ff964828ad41eabece033249d5de
JetSoler <solerjetmigs2@gmail.com>
2026-09-10 17:25:26 +0800

    Update Code_Challenge3.py

--- Code_Challenge3.py
+++ Code_Challenge3.py
@@ -1,38 +1,46 @@
 Name = input("Please Input Your Name:  ")
-
+print("-----------------------------------------------------")
 item_type = input("What is the type of items:  "  )
-
-fragile = bool(input("is it fragile?  "  ))
-
+print("-----------------------------------------------------")
+fragile = bool(input("is it fragile? True or False?  "  ))
+print("-----------------------------------------------------")
 weight = float(input("How heavy is it?  "  ))
-
+print("-----------------------------------------------------")
 distance = float(input("How far do it need to travel?  "  ))
+print("-----------------------------------------------------")
+express = bool(input("Is it rushed?  True or False?  "  ))
+print("-----------------------------------------------------")
+international = bool(input("is it international? True or False?  "  ))
+print("-----------------------------------------------------")
 
-express = bool(input("Is it rushed?  "  ))
 
-international = bool(input("is it international?  "  ))
+base_cost = (weight * 2.50) + (distance * 0.15)
 
-base_cost = 5
-costW = weight * 2.50 
-CostD = distance * 0.15
-CostE = 
 
-if weight <= 2 and distance <= 100:
-	print(" Based Cost is free  ")
+# Free Shipping
+if weight <= 2 and distance <= 100 and express == False and international == False:
+	print(" Free Shipping  ")
+	output = 0
+
+# International
+elif express == True and international == True:
+	print("express and international is applied")
+	output = (base_cost * 1.40) + 50
+
+# Express of Heavy International
+elif express == True or international == True and weight >= 20 and weight <=29.9:
+	print("Heavy and Express/International applied") 
+	output = (base_cost * 1.20) + 25
+
+# Oversized
+elif weight >= 30 or distance > 1000:
+	print("Oversized package is applied")
+	output = base_cost + 30
+
 else:
-	print(" DENIED ")
+	print(" based cost ")
+	output = base_cost
 
+print("Your total amount is", output)
 
-
-
-
-# This is the freeshipping
-#if weight <= 2 and distance <= 100 and express == False and international == False:
-	#print(" Your shipping fee is free " )
-#else:
-	#print("DENIED")
-
-#weight <= 2
-#distance <= 100
-#express = False
-#international = False
+# There's a problem, when you type false on the it becomes true. but when you enter it becomes false
